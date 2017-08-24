8205e2becf4babaff2bc536c8b1c3f6717164f1c
Maryam EKBATANI <maryamekbatani@Maryams-MacBook-Pro.local>
2017-08-24 17:03:04 -0400

    new

--- Assets/javascript/wordbank.js
+++ Assets/javascript/wordbank.js
@@ -15,6 +15,23 @@ Words.List[8]= "coffee";
 Words.List[9]= "elephant";
 Words.List[10]= "math";
 Words.List[11]= "calculus";
+Words.List[12]= "beard";
+Words.List[13]= "science";
+Words.List[14]= "glasses";
+Words.List[15]= "hangman";
+Words.List[16]= "water";
+Words.List[17]= "turkey";
+Words.List[18]= "tea";
+Words.List[19]= "responsibility";
+Words.List[20]= "turqoise";
+Words.List[21]= "teeth";
+Words.List[22]= "cup";
+Words.List[23]= "fingers";
+Words.List[24]= "elevator";
+Words.List[25]= "sunglasses";
+Words.List[26]= "rosemary";
+
+
 
 
 Words.Length=Words.List.length;
