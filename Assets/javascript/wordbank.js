5b14372797950d20857cf829f7883b8f8f6f7f3e
Maryam EKBATANI <maryamekbatani@Maryams-MacBook-Pro.local>
2017-08-24 16:52:13 -0400

    new

---
+++ Assets/javascript/wordbank.js
@@ -0,0 +1,20 @@
+Words={};
+
+Words.List=[];
+
+
+Words.List[0]= "cat";
+Words.List[1]= "dog";
+Words.List[2]= "brown";
+Words.List[3]= "blue";
+Words.List[4]= "javascript";
+Words.List[5]= "house";
+Words.List[6]= "monday";
+Words.List[7]= "tuesday";
+Words.List[8]= "coffee";
+Words.List[9]= "elephant";
+Words.List[10]= "math";
+Words.List[11]= "calculus";
+
+
+Words.Length=Words.List.length;
